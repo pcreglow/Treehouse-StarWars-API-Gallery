e9baa2f292fbf98ddfff4c3c141a1258bb718138
Phillip Creglow <phillipcreglow@msn.com>
2017-06-24 10:47:19 -0400

    Set new class of starshipsFleet in index.html, add classes starships and starship to ul and li in app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var starships = "http://swapi.co/api/starships/";
 
 $.getJSON(starships,
     function(starship) {
-        var statusHTML = '<ul>';
+        var statusHTML = '<ul class="starships">';
         $.each(starship.results, function(i, starship) {
-            statusHTML += '<li>';
+            statusHTML += '<li class="starship">';
             statusHTML += starship.name + '</li>';
             console.log(starship.name);
         }); // for each results
         statusHTML += '</ul>';
-        $('.starships').html(statusHTML)
+        $('.starshipsFleet').html(statusHTML)
     }); // end getJSON
