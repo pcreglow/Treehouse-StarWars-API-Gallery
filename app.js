5a86e21c5636f04bd77d8bb6c74454ab0b9f4c1f
Phillip Creglow <phillipcreglow@msn.com>
2017-06-23 22:15:13 -0400

    Added div with class vehicles, setup first AJAX request to SW API for vehicle names in app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("It's a trap!");
+var vehicles = "http://swapi.co/api/vehicles/";
+
+$.getJSON(vehicles,
+    function(vehicle) {
+        var statusHTML = '<ul>';
+        $.each(vehicle.results, function(i, vehicle) {
+            statusHTML += '<li>';
+            statusHTML += vehicle.name + '</li>';
+            console.log(vehicle.name);
+        });
+        statusHTML += '</ul>';
+        $('.vehicles').html(statusHTML)
+    }); // end getJSON
