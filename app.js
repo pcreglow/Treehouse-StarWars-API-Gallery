533df82226080eb50e97e951d6b00d57394d4f25
Phillip Creglow <phillipcreglow@msn.com>
2017-06-25 22:20:21 -0400

    Refactored styles.css and app.js to remove Lightbox code

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-var starships = "http://swapi.co/api/starships/";
+    var starships = "http://swapi.co/api/starships/";
 
-$.getJSON(starships,
-    function(starship) {
-        var statusHTML = '<ul class="starships">';
-        $.each(starship.results, function(i, starship) {
-            statusHTML += '<li class="starship">';
-            statusHTML += starship.name + '</li>';
-            console.log(starship.name);
-        }); // for each results
-        statusHTML += '</ul>';
-        $('.starshipsFleet').html(statusHTML);
-
-        $(".starship").click(function(event) {
-            var lightbox = '<div id="lightbox">';
-            lightbox += '<p>' + "Star Wars" + '</p>';
-            lightbox += '</div>';
-            $("body").append(lightbox);
-            $("#lightbox").show();
-            $('#lightbox').click(function(event) {
-                $("#lightbox").hide();
-            });
-        }); // end Lightbox
-    }); // end getJSON
+    $.getJSON(starships,
+        function(starship) {
+            var statusHTML = '<ul class="starships">';
+            $.each(starship.results, function(i, starship) {
+                statusHTML += '<li class="starship">';
+                statusHTML += starship.name + '</li>';
+                console.log(starship.url);
+            }); // for each results
+            statusHTML += '</ul>';
+            $('.starshipsFleet').html(statusHTML);
+        }); // end getJSON
